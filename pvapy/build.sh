a222864087a63967d2ac90023626fa3a9da7fe34
Zachary L Lentz <zlentz@slac.stanford.edu>
2017-03-14 09:22:35 -0700

    Stop using numpy so I can sync boost version with the rest of our stack. Fix config error in RPC.

--- pvapy/build.sh
+++ pvapy/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Ignore the rest of the EPICS-CPP source and use installed v4
 cd pvaPy
-export EPICS4_DIR=$EPICS4_BASE
 
 # Manual config because autoconf is broken for python 3
 # I can be reasonably sure of the config here because of how conda works
@@ -16,13 +15,16 @@ SITE="configure/CONFIG_SITE.local"
 PYINC=`python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())"`
 echo "PVA_PY_CPPFLAGS = -I$PREFIX/include -I$PYINC" >> $SITE
 if [ $PY3K == "1" ]; then
-  echo "PVA_PY_LDFLAGS = -L/usr/lib64 -L$PREFIX/lib -lboost_python3 -lboost_numpy -l$(basename $PYINC)" >> $SITE
+  #echo "PVA_PY_LDFLAGS = -L/usr/lib64 -L$PREFIX/lib -lboost_python3 -lboost_numpy -l$(basename $PYINC)" >> $SITE
+  echo "PVA_PY_LDFLAGS = -L/usr/lib64 -L$PREFIX/lib -lboost_python3 -l$(basename $PYINC)" >> $SITE
 else
-  echo "PVA_PY_LDFLAGS = -L/usr/lib64 -L$PREFIX/lib -lboost_python -lboost_numpy -l$(basename $PYINC)" >> $SITE
+  #echo "PVA_PY_LDFLAGS = -L/usr/lib64 -L$PREFIX/lib -lboost_python -lboost_numpy -l$(basename $PYINC)" >> $SITE
+  echo "PVA_PY_LDFLAGS = -L/usr/lib64 -L$PREFIX/lib -lboost_python -l$(basename $PYINC)" >> $SITE
 fi
 echo "PVA_API_VERSION = 450" >> $SITE
-echo "PVA_RPC_API_VERSION = 450" >> $SITE
-echo "HAVE_BOOST_NUM_PY = 1" >> $SITE
+echo "PVA_RPC_API_VERSION = 440" >> $SITE
+#echo "HAVE_BOOST_NUM_PY = 1" >> $SITE
+echo "HAVE_BOOST_NUM_PY = 0" >> $SITE
 make
 
 # Drop compiled pvaccess.so file into site-packages
